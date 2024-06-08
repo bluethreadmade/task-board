9b746c2b6f2e0b3940b453461e3ab88626a23cb1
Megan McNeill <mcneim2@gmail.com>
2024-06-08 14:48:59 -0400

    submit sends task object to tasks array in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,60 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 const taskForm = $('#taskForm');
+//let tasks;
+const taskTitleInputEl = $('#task-title-input');
+const taskDescriptionInputEl = $('#description-input');
+const taskDueEl = $('#task-due');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    //let nextId = crypto.randomUUID();
+    let id = crypto.randomUUID();
+    console.log(id);
 }
 
 //test function
 function test() {
     console.log("test");
 }
+
+// TODO: Create a function that accepts an array of projects, stringifys them, and saves them in localStorage.
+function saveTasksToStorage(event) {
+    event.preventDefault();
+    
+    let tasks = taskList || [];
+
+    const task = {
+       Title: taskTitleInputEl.val(),
+       Description: taskDescriptionInputEl.val(),
+       Due: taskDueEl.val(),
+    };
+  
+    // push task object into the tasks array
+    tasks.push(task);
+  
+    // clear the input boxes
+    taskTitleInputEl.val('');
+    taskDescriptionInputEl.val('');
+    taskDueEl.val('');
+  
+    // send the array to local storage
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+
+    console.log(tasks);
+  };
+
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-    // create a new task card
-    //const taskCard = $('<div class="card project-card draggable my-3" </div>');
+// function createTaskCard(task) {
+//     // create a new task card
+//     const taskCard = "task card";
 
-    console.log("create task card");
-    // create portions of the task card
+//     console.log("create task card");
+//     // create portions of the task card
 
-}
+//     // return task card
+//     return taskCard;
+
+// }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -46,11 +81,11 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     // initialize date picker
-    $('#taskDueDate').datepicker({
+    $('#task-due').datepicker({
         changeMonth: true,
         changeYear: true,
       });
     
     // event listener for submit button
-    taskForm.on('submit', test);
+    taskForm.on('submit', saveTasksToStorage);
 });
