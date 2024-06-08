87d2e08220fd9138d4eda868499818eff01e955e
Megan McNeill <mcneim2@gmail.com>
2024-06-08 12:54:32 -0400

    submit button functions, moved button div into form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,24 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Data
-// var myModal = new bootstrap.Modal(document.getElementById('formModal'), {
-//     keyboard: false
-//   });
+const taskForm = $('#taskForm');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     //let nextId = crypto.randomUUID();
 }
 
+//test function
+function test() {
+    console.log("test");
+}
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    // create a new task card
+    //const taskCard = $('<div class="card project-card draggable my-3" </div>');
+
+    console.log("create task card");
+    // create portions of the task card
 
 }
 
@@ -39,8 +45,12 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    // initialize date picker
     $('#taskDueDate').datepicker({
         changeMonth: true,
         changeYear: true,
       });
+    
+    // event listener for submit button
+    taskForm.on('submit', test);
 });
